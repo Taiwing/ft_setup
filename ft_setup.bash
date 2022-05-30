604660abe05929bb87b7ed1ce420271463e03cfb
Yoann FOREAU <yforeau@e2r12p8.clusters.42paris.fr>
2022-05-30 11:57:41 +0200

    feat: install brew in semi non-interactive mode

---
+++ ft_setup.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+yes "" | INTERACTIVE="yes" /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#curl https://sh.rustup.rs -sSf | sh
+#brew install tmux
+#cargo install bat rg
+
+#yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+#rustup self uninstall
